103136766327a0adb55fb63c7ce9fa0caaf97d69
PavelDutka <pavel.dutka05gmail.com>
2023-04-26 13:17:43 +0200

    .

---
+++ CypherComunication/CypherComunication/Desifrovani.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.Cryptography;
+
+byte[] key = ...; // klíč
+byte[] iv = ...; // vektor
+byte[] encrypted = ...; // vstupní data
+byte[] received; // výstupní data - text
+​
+using (Aes aesAlg = Aes.Create())
+{
+    aesAlg.Key = key;
+    aesAlg.IV = iv;
+    ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+    using (MemoryStream msDecrypt = new MemoryStream(encrypted))
+    {
+        using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+        {
+            using (BinaryReader srDecrypt = new BinaryReader(csDecrypt))
+            {
+                received = srDecrypt.ReadBytes((int)msDecrypt.Length);
+            }
+        }
+    }
+}
+​
+foreach (var x in data)
+{
+    Console.Write(String.Format("{0:x2} ", x));
+}
---
+++ CypherComunication/CypherComunication/Sifrovani.cs
@@ -0,0 +1,29 @@
+﻿using System.Security.Cryptography;
+
+byte[] key = ...; // klíč
+byte[] iv = ...; // vektor
+byte[] data = ...; // vstupní data - text
+byte[] encrypted; // výstupní data
+​
+using (Aes aesAlg = Aes.Create())
+{
+    aesAlg.Key = key;
+    aesAlg.IV = iv;
+    ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+    using (MemoryStream msEncrypt = new MemoryStream())
+    {
+        using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+        {
+            using (BinaryWriter swEncrypt = new BinaryWriter(csEncrypt))
+            {
+                swEncrypt.Write(original);
+            }
+            encrypted = msEncrypt.ToArray();
+        }
+    }
+}
+​
+foreach (var x in encrypted)
+{
+    Console.Write(String.Format("{0:x2} ", x));
+}
